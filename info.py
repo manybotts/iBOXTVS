83e12af3989148cc65e590ff347b2cf6b5f4abdd
manybotts <105394368+manybotts@users.noreply.github.com>
2024-05-08 20:50:20 +0300

    Update info.py

--- info.py
+++ info.py
@@ -93,7 +93,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Shortlink Info
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'BgnavQ2FIXamyq8sgdF7aPkhLgA3)
+SHORTLINK_API = environ.get('SHORTLINK_API', 'BgnavQ2FIXamyq8sgdF7aPkhLgA3')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
